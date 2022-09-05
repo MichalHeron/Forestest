d0d2f1b14a570a3934ffcf64027f64715bf43a87
Michal Czaplewski <michal.czaplewski@zalando.pl>
2022-09-05 13:08:42 +0200

    aniamtion to nav added

--- src/js/main.js
+++ src/js/main.js
@@ -12,21 +12,16 @@ const navActive = () => {
 		link.classList.remove('active')
 	}
 	navItems.onclick = e => {
-		console.log(e.target)
+		// console.log(e.target)
 		e.target.classList.add("active")
 		navItems.classList.remove('active')
 	}
-	// navLinks.forEach(link => {
-	// 	link.classList.remove('active')
-	// })
 
-	//warunek sprawdzic czy jest klasa active jezeli tak to return a jezeli nie to nadac oraz sprawdzic gdzie jest i usunac stamtad
 }
 
 burger.addEventListener('click', showNav)
 navItems.addEventListener('click', showNav)
 
-// navLinks.forEach().addEventListener('click', navActive)
 for (const link of navLinks) {
 	link.addEventListener('click', navActive)
 }
